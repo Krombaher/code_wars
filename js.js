b6bd695321e0d5d3d22c1c20768a705c95335931
Vitalij Kazuro <vitalijkazuro939@gmail.com>
2022-08-16 20:34:01 +0300

    next kata train

--- js.js
+++ js.js
@@ -28,3 +28,30 @@ function points(games) {
     }
     return sum;
 }
+
+//Quarter of the year
+
+const quarterOf = (month) => {
+    // Your code here
+    if (month >= 1 && month <= 3) {
+        return 1;
+    } else if (month >= 4 && month <= 6) {
+        return 2;
+    } else if (month >= 7 && month <= 9) {
+        return 3;
+    } else {
+        return 4;
+    }
+
+}
+
+const quarterOf = m => Math.ceil(m / 3);
+
+
+//Convert number to reversed array of digits
+
+function digitize(n) {
+    //code here
+
+    return n.toString().split('').reverse().map(Number)
+}
